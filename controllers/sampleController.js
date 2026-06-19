28c0b5198b220398bef3c0bfc102ab2564706032
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-06-19 12:39:08 +0200

    INIT template generico di express

---
+++ controllers/sampleController.js
@@ -0,0 +1,123 @@
+import connection from "../db/connections/connection.js";
+
+async function index(request, response) {
+    try {
+        const { } = request.query;
+
+        const querySql = ``;
+
+        const [] = await connection.execute(querySql, []);
+
+        return response.status(200).json({
+            error: null,
+            data: risultati
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        return response.status(500).json({
+            error: 'Internal Server Error',
+            message: 'Errore durante il recupero dei prodotti',
+        });
+    }
+};
+
+async function show(request, response) {
+    try {
+        const { } = request.params;
+
+        const querySql = ``;
+
+        const [] = await connection.execute(querySql, []);
+
+        return response.status(200).json({
+            error: null,
+            data: risultati
+        })
+
+    } catch (error) {
+        console.error(error);
+
+        return response.status(500).json({
+            error: 'Internal Server Error',
+            message: 'Errore durante il recupero dei prodotti',
+        });
+    }
+}
+
+async function create(request, response) {
+    try {
+        const { } = request.body;
+
+        const querySql = ``;
+
+        const [] = await connection.execute(querySql, []);
+
+        return response.status(201).json({
+            error: null,
+            message: 'Creato con successo',
+            data: { id: risultato.insertId } 
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        return response.status(500).json({
+            error: 'Internal Server Error',
+            message: 'Errore durante la creazione'
+
+        });
+    }
+};
+
+async function modify(request, response) {
+    try {
+        const {} = request.params; // per l'id
+        const {} = request.body;   // corpo modifica
+
+        const query = ``;
+
+        const [] = await connection.execute(query, []);
+
+        return response.status(200).json({
+            error: null,
+            message: 'Modificato con successo',
+            data: { id: parseInt(id) }
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        return response.status(500).json({
+            error: 'Internal Server Error',
+            message: 'Errore durante la modifica',
+        });
+    }
+};
+
+async function destroy(request, response) {
+    try {
+        const {} = request.params;
+
+        const querySql = ``;
+
+        const [] = await connection.execute(querySql);
+
+        return response.status(200).json({
+            error: null,
+            message: 'Eliminato con successo',
+            data: { id: parseInt(id) }
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        return response.status(500).json({
+            error: 'Internal Server Error',
+            message: 'Errore durante l\'eliminazione',
+        });
+    }
+}
+
+export { index, show, create, modify, destroy };
---
+++ routers/sampleRouter.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+import { index, show, create, modify, destroy } from '../controllers/samplesController.js';
+
+const sampleRouter = express.Router();
+
+// INDEX http://localhost:3000/sample
+sampleRouter.get('/', index);
+
+// SHOW http://localhost:5555/sample/:id
+sampleRouter.get('/:id', show);
+
+// CREATE http://localhost:5555/sample
+sampleRouter.post('/', create)
+
+// PATCH http://localhost:5555/sample/:id
+sampleRouter.patch('/:id', modify)
+
+// DELETE http://localhost:5555/sample/:id
+sampleRouter.delete('/:id', destroy)
+
+export default sampleRouter;
