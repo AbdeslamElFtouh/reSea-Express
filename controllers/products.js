59dfc2f1340322cb53a506c7d70a44500d388083
Francesco Cassese <francescocassese9@gmail.com>
2026-06-20 17:13:21 +0200

    fix: aggiunta parte mancante al commit precedente

--- controllers/products.js
+++ controllers/products.js
@@ -24,30 +24,9 @@ async function index(request, response) {
         // baseURL='http://localhost:3000'    
         const baseUrl = `${request.protocol}://${request.get('host')}`;
 
-        // Utilizzo .map() per creare un nuovo array trasformando ogni singolo oggetto 'product'
-        const productsFormatted = rows.map(product => {
-
-            //Assegno alla variabile il "nome dell'immagine" e gli do un placeholder in caso di immagine inesistente 
-            const imageFileName = product.image ? product.image : "placeholder.png";
-
-            //Estraggo create_date dall'oggetto JSON per non farlo arrivare alla card. Non sono dati 
-            //che interessano al cliente
-            const { create_date, ...productData } = product;
-
-            return {
-                //restituisco l'altra parte dell'oggetto
-                ...productData,
-
-                //trasformo sia prezzo che plastic_offset_kg per poi poterci fare
-                //operazioni in futuro aggiungendo 0 in caso che arrivi una stringa vuota
-                price: parseFloat(product.price) || 0,
-                plastic_offset_kg: parseFloat(product.plastic_offset_kg) || 0,
-
-                //qui viene montato l'URL completo delle immagini, così al momento della richiesta gli viene passato tutto l'URL
-                // 'http://localhost:3000/assets/nome-img.jpg'
-                image: `${baseUrl}/assets/${imageFileName}`
-            };
-        })
+        // Utilizzo .map() per creare un nuovo array trasformando ogni singolo oggetto 'product' e passando ogni singolo oggetto alla funzione
+        // insieme alla base recuperata in precedenza, il resto lo troverete in utils\utils.js
+        const productsFormatted = rows.map(product => formatProduct(product, baseUrl));
 
         return response.status(200).json({
             error: null,
@@ -100,9 +79,13 @@ async function show(request, response) {
             });
         }
 
+        //stessa cosa di prima, solo che qui alla funzione passo rows[0] che è il primo elemento dell'array
+        const baseUrl = `${request.protocol}://${request.get('host')}`;
+        const productFormatted = formatProduct(rows[0], baseUrl);
+
         return response.status(200).json({
             error: null,
-            data: rows[0]
+            data: productFormatted
         });
 
     } catch (error) {
